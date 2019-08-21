5f757eadab7c54574c5c39f7ddd3c5dcfe2c3e6a
Axinacis <123GittiKukkeli>
2019-08-21 13:55:35 +0300

    Changes for testing production version

--- src/app.js
+++ src/app.js
@@ -1,51 +1,52 @@
-const express = require('express')
-const path = require('path')
-const request = require('request')
-const geocode = require('./geocode.js')
-const forecast = require('./forecast.js')
-const app = express()
+const path = require('path');
+const request = require('request');
+const geocode = require('./geocode.js');
+const forecast = require('./forecast.js');
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.send('Etusihvu')
-})
+    res.send('Etusihvu')
+});
 
 app.get('/about', (req, res) => {
-  res.send({
-    forecast: 'Sataa lunta',
-    location: 'Joensuu'
-  })
-})
+    res.send({
+        forecast: 'Sataa lunta',
+        location: 'Joensuu'
+    })
+});
 
 app.get('/weather', (req, res) => {
-  if (!req.query.address) {
-    return res.send({
-      error: 'Virhelöinen tapahtunut on, paikkeloinen pitäisi ollak'
-    })
-  }
-  geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
-    if (error) {
-      return res.send({error})
+    if (!req.query.address) {
+        return res.send({
+            error: 'Virhelöinen tapahtunut on, paikkeloinen pitäisi ollak'
+        })
     }
-    forecast(latitude, longitude, (error, data) => {
-      if (error) {
-        return res.send({error})
-      }
-      res.send({
-        forecast: data,
-        location,
-        address: req.query.address
-      })
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, data) => {
+            if (error) {
+                return res.send({error})
+            }
+            res.send({
+                forecast: data,
+                location,
+                address: req.query.address
+            })
+        })
     })
-  })
-})
+});
 
 
 app.get('*', (req, res) => {
-  res.redirect('/');
+    res.redirect('/');
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000')
-})
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+});
